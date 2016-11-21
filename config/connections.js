44a11f2f0dad0f1d74369151a15b63c27d225792
Pavel Zubkou <pavel.zubkou@gmail.com>
2016-11-21 20:09:36 +0300

    Configure sails to use mLab database

--- config/connections.js
+++ config/connections.js
@@ -56,6 +56,10 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
+  mLabMongodbServer: {
+    adapter: 'sails-mongo',
+    url: process.env.MLAB_URI,
+  },
   // someMongodbServer: {
   //   adapter: 'sails-mongo',
   //   host: 'localhost',
